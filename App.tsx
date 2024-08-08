d5aa9b1f84442b03845cbcdb239a3140af5b807b
Bregza1309 <107304898+Bregza1309@users.noreply.github.com>
2024-08-08 12:09:02 +0200

    update

--- App.tsx
+++ App.tsx
@@ -1,13 +1,14 @@
-import { Text, View , StatusBar } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 import styles from './styles';
 import { Box } from './Box';
 export default function App() {
+  const boxes = new Array(10).fill(null).map((v, i) => i + 1);
   return (
     <View style={styles.container}>
-      <Box>#1</Box>
-      <Box>#2</Box>
+      <StatusBar hidden={false} />
+      {boxes.map((b) => (
+        <Box key={b}>#{b}</Box>
+      ))}
     </View>
   );
 }
-
-
